650e710d561d0e78420be813249c92ea9a2d13f2
Tony Arcieri <tony@iqlusion.io>
2018-04-12 21:22:45 -0700

    Export HttpConfig from crate toplevel
    Makes it easier to get to, and it's one of the main structs of interest
    to an end-user.

--- src/lib.rs
+++ src/lib.rs
@@ -89,7 +89,7 @@ pub mod session;
 
 pub use algorithm::Algorithm;
 pub use capabilities::Capabilities;
-pub use connector::{Connector, HttpConnector};
+pub use connector::{Connector, HttpConfig, HttpConnector};
 pub use domains::Domains;
 pub use object::Id as ObjectId;
 pub use object::Label as ObjectLabel;
