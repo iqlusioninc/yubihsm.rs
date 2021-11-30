bac7e3e33bdbcd65aa5dd9af715e929c1ae32b25
Tony Arcieri (iqlusion) <tony@iqlusion.io>
2021-11-30 06:39:23 -0800

    v0.40.0-pre (#276)

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/iqlusioninc/yubihsm.rs/main/img/logo.png",
-    html_root_url = "https://docs.rs/yubihsm/0.39.0"
+    html_root_url = "https://docs.rs/yubihsm/0.40.0-pre"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![forbid(unsafe_code)]
