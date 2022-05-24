75bc47fe9b24882a4341a6754504f277b5a0cefc
Tony Arcieri <tony@iqlusion.io>
2022-05-24 14:12:30 -0600

    Bump `uuid` to v1.0

--- src/uuid.rs
+++ src/uuid.rs
@@ -1,16 +1,12 @@
 //! UUID functionality
 
 use rand_core::{OsRng, RngCore};
+use uuid::Builder;
 pub use uuid::Uuid;
-use uuid::{Builder, Variant, Version};
 
 /// Create a random UUID
 pub fn new_v4() -> Uuid {
     let mut bytes = [0; 16];
     OsRng.fill_bytes(&mut bytes);
-
-    Builder::from_bytes(bytes)
-        .set_variant(Variant::RFC4122)
-        .set_version(Version::Random)
-        .build()
+    Builder::from_random_bytes(bytes).into_uuid()
 }
