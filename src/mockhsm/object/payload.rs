c073d84dc4df9f0de9a00f7d787c9244bcf23128
Arthur Gautier <arthur.gautier@arista.com>
2023-12-01 14:03:35 -0800

    mockhsm: adds support for rsa in `put_asymmetric_key`

--- src/mockhsm/object/payload.rs
+++ src/mockhsm/object/payload.rs
@@ -4,7 +4,9 @@
 use crate::{algorithm::Algorithm, asymmetric, authentication, hmac, opaque, wrap};
 use ecdsa::elliptic_curve::sec1::ToEncodedPoint;
 use ed25519_dalek as ed25519;
+use num_traits::cast::FromPrimitive;
 use rand_core::{OsRng, RngCore};
+use rsa::{traits::PublicKeyParts, BigUint};
 
 /// Loaded instances of a cryptographic primitives in the MockHsm
 #[derive(Debug)]
@@ -20,6 +22,9 @@ pub(crate) enum Payload {
 
     /// Ed25519 signing key
     Ed25519Key(ed25519::SigningKey),
+
+    /// Rsa private key
+    RsaKey(rsa::RsaPrivateKey),
 
     /// HMAC key
     HmacKey(hmac::Algorithm, Vec<u8>),
@@ -48,6 +53,17 @@ impl Payload {
                 asymmetric::Algorithm::Ed25519 => {
                     assert_eq!(data.len(), ed25519::SECRET_KEY_LENGTH);
                     Payload::Ed25519Key(ed25519::SigningKey::try_from(data).unwrap())
+                }
+                asymmetric::Algorithm::Rsa2048
+                | asymmetric::Algorithm::Rsa3072
+                | asymmetric::Algorithm::Rsa4096 => {
+                    assert_eq!(data.len(), asymmetric_alg.key_len());
+                    let exp = BigUint::from_u64(65537).expect("invalid static exponent");
+                    let p = BigUint::from_bytes_be(&data[..asymmetric_alg.key_len() / 2]);
+                    let q = BigUint::from_bytes_be(&data[asymmetric_alg.key_len() / 2..]);
+
+                    let key = rsa::RsaPrivateKey::from_p_q(p, q, exp).unwrap();
+                    Payload::RsaKey(key)
                 }
                 _ => {
                     panic!("MockHsm doesn't support this asymmetric algorithm: {asymmetric_alg:?}")
@@ -102,6 +118,12 @@ impl Payload {
             Payload::EcdsaNistP256(_) => Algorithm::Asymmetric(asymmetric::Algorithm::EcP256),
             Payload::EcdsaSecp256k1(_) => Algorithm::Asymmetric(asymmetric::Algorithm::EcK256),
             Payload::Ed25519Key(_) => Algorithm::Asymmetric(asymmetric::Algorithm::Ed25519),
+            Payload::RsaKey(ref k) => match k.size() {
+                256 => Algorithm::Asymmetric(asymmetric::Algorithm::Rsa2048),
+                384 => Algorithm::Asymmetric(asymmetric::Algorithm::Rsa3072),
+                512 => Algorithm::Asymmetric(asymmetric::Algorithm::Rsa4096),
+                other => panic!("MockHsm doesn't support rsa key size {} bits", other * 8),
+            },
             Payload::HmacKey(alg, _) => alg.into(),
             Payload::Opaque(alg, _) => alg.into(),
             Payload::WrapKey(alg, _) => alg.into(),
@@ -114,6 +136,7 @@ impl Payload {
             Payload::AuthenticationKey(_) => authentication::key::SIZE,
             Payload::EcdsaNistP256(_) | Payload::EcdsaSecp256k1(_) => 32,
             Payload::Ed25519Key(_) => ed25519::SECRET_KEY_LENGTH,
+            Payload::RsaKey(k) => k.size(),
             Payload::HmacKey(_, ref data) => data.len(),
             Payload::Opaque(_, ref data) => data.len(),
             Payload::WrapKey(_, ref data) => data.len(),
@@ -131,6 +154,7 @@ impl Payload {
                 Some(secret_key.public_key().to_encoded_point(false).as_bytes()[1..].into())
             }
             Payload::Ed25519Key(signing_key) => Some(signing_key.verifying_key().to_bytes().into()),
+            Payload::RsaKey(private_key) => Some(private_key.n().to_bytes_be()),
             _ => None,
         }
     }
@@ -150,6 +174,34 @@ impl Payload {
             Payload::EcdsaNistP256(k) => k.to_bytes().to_vec(),
             Payload::EcdsaSecp256k1(k) => k.to_bytes().to_vec(),
             Payload::Ed25519Key(k) => k.verifying_key().to_bytes().into(),
+            Payload::RsaKey(k) => {
+                use rsa::traits::PrivateKeyParts;
+                let mut out = Vec::new();
+
+                {
+                    let primes = k.primes();
+                    // p
+                    out.extend_from_slice(&primes[0].to_bytes_be());
+                    // q
+                    out.extend_from_slice(&primes[1].to_bytes_be());
+                }
+
+                // dp
+                if let Some(dp) = k.dp() {
+                    out.extend_from_slice(&dp.to_bytes_be())
+                }
+                // dq
+                if let Some(dq) = k.dq() {
+                    out.extend_from_slice(&dq.to_bytes_be())
+                }
+                // qinv
+                // Note(baloo): The sign is just dropped here.
+                if let Some(qinv) = k.qinv() {
+                    out.extend_from_slice(&qinv.to_bytes_be().1)
+                }
+
+                out
+            }
             Payload::HmacKey(_, data) => data.clone(),
             Payload::Opaque(_, data) => data.clone(),
             Payload::WrapKey(_, data) => data.clone(),
