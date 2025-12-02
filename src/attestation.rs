9d4be40146c185a1e1afeb54a94560cda14a7641
Arthur Gautier <arthur.gautier@arista.com>
2025-12-02 11:23:47 -0800

    attestation: adds support in mockhsm (#576)

--- src/attestation.rs
+++ src/attestation.rs
@@ -1,7 +1,16 @@
 //! Attestation Certificates: generate an X.509 certificate which attests that
 //! a key generated with a YubiHSM is genuine
 
+use crate::object;
+
 mod certificate;
 pub(crate) mod commands;
+#[cfg(feature = "mockhsm")]
+mod pkix;
 
 pub use self::certificate::Certificate;
+#[cfg(feature = "mockhsm")]
+pub use self::pkix::*;
+
+/// Default attestation key ID slot
+pub const DEFAULT_ATTESTATION_KEY_ID: object::Id = 0;
