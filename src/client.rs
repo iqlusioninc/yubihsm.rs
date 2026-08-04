aa01308267f884b570a2c0c66ca55f61f88d6775
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2026-08-04 22:33:11 +0200

    Add `std::fmt::Debug` implementation to `Client` and `Connector` (#660)
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- src/client.rs
+++ src/client.rs
@@ -1217,3 +1217,11 @@ impl Client {
             .0)
     }
 }
+
+impl std::fmt::Debug for Client {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Client")
+            .field("connector", &self.connector)
+            .finish_non_exhaustive()
+    }
+}
